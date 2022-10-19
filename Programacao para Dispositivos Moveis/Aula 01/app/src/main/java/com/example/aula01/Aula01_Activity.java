2df83e1bf0e18a8c93a75298da3f6907170f49bd
GRP83 <ggrrpp83@gmail.com>
2022-10-19 01:56:44 -0300

    new activity added
    inclusao de navegacao para segunda activity

---
+++ Programacao para Dispositivos Moveis/Aula 01/app/src/main/java/com/example/aula01/Aula01_Activity.java
@@ -0,0 +1,14 @@
+package com.example.aula01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aula01_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aula01);
+    }
+}
--- Programacao para Dispositivos Moveis/Aula 01/app/src/main/java/com/example/aula01/MainActivity.java
+++ Programacao para Dispositivos Moveis/Aula 01/app/src/main/java/com/example/aula01/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.aula01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button01 = (Button) findViewById(R.id.button01);
+
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button01activity();
+            }
+        });
+    }
+
+    private void button01activity() {
+
+        startActivity(new Intent(MainActivity.this, Aula01_Activity.class));
+
     }
 }
